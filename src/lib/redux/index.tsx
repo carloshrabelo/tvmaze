e7356d846b029b57153653706ff43a04ff9857d8
Carlos Henrique Rabelo <carloshrabelo@hotmail.com>
2024-02-24 13:29:36 +0000

    chore: add redux

---
+++ src/lib/redux/index.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import { Provider } from "react-redux";
+
+import { store } from "../../store";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function ReduxProvider({ children }: Props) {
+  return <Provider store={store}>{children}</Provider>;
+}
